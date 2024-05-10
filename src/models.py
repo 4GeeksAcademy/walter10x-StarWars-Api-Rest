acc892c31a3ad2c7142f0292e037b977904e9a28
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-05-10 01:32:16 +0000

    Edit model for StarWars

--- src/models.py
+++ src/models.py
@@ -7,11 +7,11 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     is_active = db.Column(db.Boolean(), nullable=False)
-    favorite_planet = db.relationship('FavoritePlanet', backref='user', lazy=True)
-    favorite_people = db.relationship('FavoritePeople', backref='user', lazy=True)
+    favorite_planet = db.relationship('FavoritePlanet', backref='favorite_planet_user', lazy=True)
+    favorite_people = db.relationship('FavoritePeople', backref='favorite_people_user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -22,15 +22,19 @@ class User(db.Model):
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    description = db.Column(db.String)
+    image = db.Column(db.String)
+    people = db.relationship("People", backref="planet")  # Relación establecida aquí
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Planet %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "description": self.description,
+            "image": self.image,
         }
     
 class People(db.Model):
@@ -38,46 +42,50 @@ class People(db.Model):
     name = db.Column(db.String)
     description = db.Column(db.String)
     image = db.Column(db.String)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<People %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "description": self.description,
+            "image": self.image,
+            "planet_id": self.planet_id  # Agrega el id del planeta para referencia
         }
-
 class FavoritePlanet(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-   planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    user = db.relationship("User", backref="favourite_planets")
+    planet = db.relationship("Planet")
 
-   def __repr__(self):
-        return '<User %r>' % self.username
+    def __repr__(self):
+        return '<FavoritePlanet %r>' % self.id
 
-   def serialize(self):
+    def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
         }
-
 
 class FavoritePeople(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-   people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    user = db.relationship("User", backref="favourite_peoples")
+    people = db.relationship("People")
 
-   def __repr__(self):
-        return '<User %r>' % self.username
+    def __repr__(self):
+        return f'<FavoritePeople {self.id}>'
 
-   def serialize(self):
+    def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "user_id": self.user_id,
+            "people_id": self.people_id,
         }
-
     
